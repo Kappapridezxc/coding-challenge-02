31f7a54f22ca2ee34a2cce5684e255b4966fba66
exequiel.delfin1 <exequiel.delfin1@trans-cosmos.co.jp>
2023-09-19 13:45:00 +0800

    coding challege update 09/19/2023

---
+++ js/script.js
@@ -0,0 +1,32 @@
+(function() {
+  activeNumber();
+  var submitButton = document.querySelector('.submit-btn');
+  submitButton.addEventListener('click', function(){
+    var selected = getSelectedNumber();
+    if (selected == "" || selected == null) {
+      alert('Please rate us!'); 
+    }else {
+      alert(selected);
+    }
+  });
+  function activeNumber(){
+    var numberList = document.getElementsByClassName('number');
+      for (i=0; i < numberList.length; i++) {       
+        numberList[i].addEventListener('click', function(){ 
+          for (x=0; x < numberList.length; x++) {
+            numberList[x].classList.remove('active');
+          }
+          this.classList.add('active');
+      });
+    };
+  }
+  function getSelectedNumber(){
+    var selectedNumber = document.querySelector('.active')
+    if (selectedNumber == null) {
+      return null;
+    }else {
+      var number = selectedNumber.textContent;
+      return number;
+    }
+  };
+}());
